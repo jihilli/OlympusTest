522a7bfffb1df24fc17be67b2b9988e03d846d03
신지훈 <130414837+Jihoon0127@users.noreply.github.com>
2023-10-19 10:10:21 +0900

    tailwind.config 수정

--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -2,10 +2,6 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
-    fontFamily: {
-      custom1: ['Cafe24', 'Cafe24'],
-      custom2: ['Neo', 'Neo'],
-    },
     extend: {
       screens: {
         xs: '320px',
@@ -20,7 +16,7 @@ module.exports = {
         'custom-blue': '#1E1E1E',
         'custom-white': '#fff',
         'custom-black': '#000000',
-        'custom-black2': '#121212',
+        'custom-black2': '#202020',
         'custom-gray': '#A0A0A0',
         'custom-darkgray': 'rgba(217, 217, 217, 0.10)',
         'custom-lightpurple': '#C0CBE7',
@@ -28,6 +24,7 @@ module.exports = {
         'custom-lightgray': '#D9D9D9',
         'custom-background':
           'bg-gradient-to-t from-gray-900 via-stone-800 to-gray-400',
+        'custom-pink': '#E63A87',
       },
     },
   },
