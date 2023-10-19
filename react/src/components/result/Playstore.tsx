f3d94a84131b530a2d3e0c01967f163c03adc2a7
jihee <jihilli@naver.com>
2023-10-19 10:12:51 +0900

    성향 설명 개행

---
+++ react/src/components/result/Playstore.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Playstore = () => {
+    return (
+        <div>
+            {/* <div>
+                <button
+                    onClick={() =>
+                        window.open('https://play.google.com/store/apps/details?id=com.indj.music', '_blank')
+                    }
+                    className="mt-4 px-4 py-2 bg-green-500 text-white rounded mr-10"
+                >
+                    Google Play Store
+                </button>
+                <button
+                    onClick={() =>
+                        window.open(
+                            'https://apps.apple.com/kr/app/indj-%EC%9E%90%EC%9C%A0%EB%A1%AD%EA%B2%8C-%EB%93%A3%EB%8A%94-%EC%83%81%ED%99%A9-%EA%B0%90%EC%84%B1-%EC%9D%B8%EA%B3%B5%EC%A7%80%EB%8A%A5-%EC%9D%8C%EC%95%85/id1513542512',
+                            '_blank'
+                        )
+                    }
+                    className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+                >
+                    App Store
+                </button>
+            </div> */}
+        </div>
+    );
+};
+
+export default Playstore;
